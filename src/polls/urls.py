049e32d62da910ce54914728bdb13ed2a7c4ad37
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-02-03 17:49:13 -0300

    Following part 4 with views as methods

--- src/polls/urls.py
+++ src/polls/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/', detail, name='detail'),
     path('<int:question_id>/vote', vote, name='vote'),
-    path('<int:question_id>/results', results, name='resuts'),
+    path('<int:question_id>/results', results, name='results'),
 ]
