8aeb49ca88d017a41928599cfafb8b805d977e5e
Jean Barbosa <jeanbarbosa9@gmail.com>
2021-02-04 20:03:48 -0300

    Following part 5, testing question model method Basically the was_published_recently method

--- src/polls/tests.py
+++ src/polls/tests.py
@@ -1,3 +1,30 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+
+class TestQuestionModel(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """It should return False if a question was published in the future."""
+        future_date = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=future_date)
+
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        """It should return False if a question was published more than 1 day ago."""
+
+        old_date = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_date)
+
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        """It should return True if a question was published until 1 day ago."""
+
+        recent_date = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=recent_date)
+
+        self.assertIs(recent_question.was_published_recently(), True)
